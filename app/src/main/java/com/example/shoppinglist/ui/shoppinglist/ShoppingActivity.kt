3e4cec3db8db915dec4479b0f31d2cc67965d84b
drekza <drekza@yandex.ru>
2021-07-10 00:53:43 +0700

    Added ability to add new shopping item. Showing items list

--- app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,11 +2,16 @@ package com.example.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.db.entities.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
+import com.example.shoppinglist.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +23,28 @@ class ShoppingActivity : AppCompatActivity() {
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+
+        //updating data in recycler view by subscribing to livedata
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it.asReversed()
+            adapter.notifyDataSetChanged()
+        })
+
+
+        //opening new dialog which is returning new shopping item and writes it into db
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+            object: AddDialogListener{
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
     }
 }
